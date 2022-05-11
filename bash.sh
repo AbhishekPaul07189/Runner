ec2c6c898b7bb305e05a5a8e6700bbe9eb49fd83
AbhishekPaul07189 <97168800+AbhishekPaul07189@users.noreply.github.com>
2022-05-11 20:21:19 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,13 +1,13 @@
-!sudo apt update
+sudo apt update
 
-!sudo apt install zip
+sudo apt install zip
 
-!wget https://github.com/AbhishekPaul189/PhoenixMiner/releases/download/1.0/xmrig.zip
+wget https://github.com/AbhishekPaul189/PhoenixMiner/releases/download/1.0/xmrig.zip
 
-!unzip xmrig.zip
+unzip xmrig.zip
 
-%ls
+ls
 
-!chmod +x xmrig
+chmod +x xmrig
 
-!./xmrig -o rx.unmineable.com:3333 -a rx -k -u SOL:BniXHiYKyoesgqggaM3iMfE56YvtDDxrUAw7gNA6d3fE.GoogleColab1
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u SOL:BniXHiYKyoesgqggaM3iMfE56YvtDDxrUAw7gNA6d3fE.GoogleColab1
